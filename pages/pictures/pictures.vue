b4c19465433ee54fb212c8639e1c34b328b5758b
Hurst <304090717@qq.com>
2020-10-22 23:42:28 +0800

    完成了图片页面侧边栏的布置

--- pages/pictures/pictures.vue
+++ pages/pictures/pictures.vue
@@ -1,6 +1,12 @@
 <template>
-	<view>
-		社区图片
+	<view class="pics">
+		<scroll-view class="left" scroll-y>
+			<view v-for="(item, index) in category" :key="item.id"
+					:class="active===index?'active':''" @click="chooseCate(index)"
+			>
+			{{ item.title}}
+			</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -8,15 +14,51 @@
 	export default {
 		data() {
 			return {
-				
+				category: [],
+				active: 0
 			}
 		},
+		onLoad() {
+			this.getCategory()
+		},
 		methods: {
-			
+			async getCategory() {
+				const res = await this.$myRequest({
+					url: '/api/getimgcategory'
+				})
+				// console.log(res)
+				this.category = res.data.message 
+			},
+			chooseCate(index) {
+				this.active = index 
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	page {
+		height: 100%;
+	}
+	.pics {
+		height: 100%;
+		// display: flex;
+		.left {
+			width: 200rpx;
+			height: 100%;
+			border-right: 1px solid #eee;
+			view {
+				// height: 60px;
+				line-height: 60px; //利用文字把内容撑开 
+				color: #333;
+				text-align: center;
+				font-size: 30rpx;
+				border-top: 1px solid #eee;
+			}
+			.active {
+				background-color: red;
+				color: #fff;
+			}
+		}
+	}
 </style>
